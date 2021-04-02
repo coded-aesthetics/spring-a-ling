0140d54e95b89ac3873df2b6f5f50f5e20f3e4f5
Jan Reinsch <reinsch.jan@gmail.com>
2021-04-02 21:10:31 +0200

    more springs and a particle gun

--- sketch.js
+++ sketch.js
@@ -5,19 +5,21 @@ let then = Date.now();
 let spring;
 let spring2;
 let spring3;
+let spring4;
 
 const fr = 60.0;
 const surface_friction = 2.5;
 
 function setup() {
-  createCanvas(1000, 1000);
+  createCanvas(window.innerWidth, window.innerHeight);
   frameRate(fr);
 
-  spring = new Spring(new Particle(Math.random()* 1000, Math.random()* 1000), Math.random());
+  spring = new Spring(new Particle(Math.random()* window.innerWidth, Math.random()* window.innerHeight), Math.random() * 0.1);
 
-  spring2 = new Spring(new Particle(Math.random()* 1000, Math.random()* 1000), Math.random());
-  spring3 = new Spring(new Particle(Math.random()* 1000, Math.random()* 1000), Math.random());
-  particle = new Particle(600, 600, createVector(Math.random()* 1000 - 500, Math.random()* 1000 -500));
+  spring2 = new Spring(new Particle(Math.random()* window.innerWidth, Math.random()* window.innerHeight), Math.random() * 0.1);
+  spring3 = new Spring(new Particle(Math.random()* window.innerWidth, Math.random()* window.innerHeight), Math.random() * 0.1);
+  spring4 = new Spring(new Particle(Math.random()* window.innerWidth, Math.random()* window.innerHeight), Math.random() * 0.1);
+  particle = new Particle(Math.random()* window.innerWidth, Math.random()* window.innerHeight, createVector(Math.random()* window.innerWidth - window.innerWidth/2, Math.random()* 1000 -500));
 }
 
 function Particle(x_, y_, velocity_ = createVector(0,0)) {
@@ -33,6 +35,19 @@ function Particle(x_, y_, velocity_ = createVector(0,0)) {
     this.velocity.mult(1- ((1 - friction) * time_slice));
     // this.velocity.mult(friction);
     this.pos.add(p5.Vector.mult(this.velocity, time_slice));
+  }
+  this.emit_particles = function(amount) {
+    const particles = [];
+    for (let i = 0; i < amount; i++) {
+      particles.push(
+        new Particle(this.pos.x, this.pos.y,
+          p5.Vector.add(this.velocity, createVector(
+            Math.random() * 1000 - 500,
+            Math.random() * 1000 - 500)
+          ))
+        );
+    }
+    return particles;
   }
 }
 
@@ -66,6 +81,14 @@ function drawParticleFunk(particle) {
   ellipse(particle.pos.x, particle.pos.y, radius, radius)
 }
 
+function drawParticleFunk2(particle) {
+  stroke(0, 0, 0, 1.0);
+  const radius = particle.velocity.mag() / 10;
+  // Math.random() > 0.5 ? fill(radius * 5, 0 ,radius * 5) : fill(0, radius * 5 ,0);
+  fill(Math.round(Math.random() * 255), Math.round(Math.random() * 255), Math.round(Math.random() * 255));
+  ellipse(particle.pos.x, particle.pos.y, radius, radius)
+}
+
 
 function drawSpring(spring) {
   drawParticle(spring.particle_center);
@@ -76,6 +99,8 @@ function drawLine(p, p2) {
   line(p.pos.x, p.pos.y, p2.pos.x, p2.pos.y);
 }
 
+let particles = [];
+
 function draw() {
   const deltaTime = (Date.now() - then);
   background(0);
@@ -85,14 +110,32 @@ function draw() {
   spring.act_on_particle(particle, time_slice);
   spring2.act_on_particle(particle, time_slice);
   spring3.act_on_particle(particle, time_slice);
+  spring4.act_on_particle(particle, time_slice);
 
   particle.update(time_slice, 1 / surface_friction);
   drawParticleFunk(particle);
   drawSpring(spring);
   drawSpring(spring2);
   drawSpring(spring3);
+  drawSpring(spring4);
   drawLine(spring.particle_center, particle);
   drawLine(spring2.particle_center, particle);
   drawLine(spring3.particle_center, particle);
+  drawLine(spring4.particle_center, particle);
+
+
+  particles.forEach(p => p.update(time_slice, 1 / surface_friction))
+  particles.forEach(drawParticleFunk)
+
   then = Date.now();
+}
+
+function keyPressed() {
+  console.log(keyCode);
+  if (keyCode === ENTER) {
+    particles = particles.concat(particle.emit_particles(18));
+  }
+  if (keyCode === 32) {
+    particles = [];
+  }
 }
